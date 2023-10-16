9be9de7fc0a66af415d4cdfebefe926f2b693ee4
wuzhf9 <846461609@qq.com>
2023-10-16 21:21:36 +0800

    add allreduce in metric

--- mindocr/metrics/kie_metrics.py
+++ mindocr/metrics/kie_metrics.py
@@ -1,5 +1,8 @@
 from seqeval.metrics import f1_score, precision_score, recall_score
 
+import mindspore as ms
+from mindspore import Tensor
+
 from ..utils.misc import AllReduce
 
 __all__ = ["VQASerTokenMetric"]
@@ -25,9 +28,20 @@ class VQASerTokenMetric:
         self.gt_list.extend(labels)
 
     def eval(self):
+        precision = Tensor(precision_score(self.gt_list, self.pred_list), dtype=ms.float32)
+        recall = Tensor(recall_score(self.gt_list, self.pred_list), dtype=ms.float32)
+        hmean = Tensor(f1_score(self.gt_list, self.pred_list), dtype=ms.float32)
+        if self.all_reduce:
+            precision = float((self.all_reduce(precision) / self.device_num).asnumpy())
+            recall = float((self.all_reduce(recall) / self.device_num).asnumpy())
+            hmean = float((self.all_reduce(hmean) / self.device_num).asnumpy())
+        else:
+            precision = float(precision.asnumpy())
+            recall = float(recall.asnumpy())
+            hmean = float(hmean.asnumpy())
         metrics = {
-            "precision": precision_score(self.gt_list, self.pred_list),
-            "recall": recall_score(self.gt_list, self.pred_list),
-            "hmean": f1_score(self.gt_list, self.pred_list),
+            "precision": precision,
+            "recall": recall,
+            "hmean": hmean
         }
         return metrics
